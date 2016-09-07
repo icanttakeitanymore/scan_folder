59b95433779f5a8a1f26089c8da7362f7e861cec
icanttakeitanymore <polozovbv@gmail.com>
2016-09-07 10:55:47 +0300

    Create scan_folder.py

---
+++ scan_folder.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import os
+import time
+
+TIME = 1
+FILELIST = None
+PATH_FOR_LINKS = "/home/boris/links_folder"
+PATH_FOR_SCAN = "/home/boris/scac_folder"
+
+def link_founder(PATH_FOR_LINKS):
+	"""
+	Поиск существующихлинков. Для того чтобы понимать какие линки создавать.
+	"""
+	CREATED_LINKS = {directory:files for directory,x, files in os.path(PATH_FOR_LINKS)}
+	for directory in CREATED_LINKS.keys():
+		for file in CREATED_LINKS[directory]
+			yield "{DIR}/{FILE}".format(DIR=directory,FILE=file)
+
+def scan_folder(PATH_FOR_SCAN):
+	"""
+	Поиск файлов в директории.
+	"""
+	FILES_FOUND = {directory:files for directory,x, files in os.path(PATH_FOR_SCAN)}
+	for directory in FILES_FOUND.keys():
+		for file in FILES_FOUND[directory]
+			yield "{DIR}/{FILE}".format(DIR=directory,FILE=file)
+
+	
+if __name__ == "__main__":
+	print("Для текущих файлов линки существуют")
+	links = list(link_founder)
+	files = list(scac_folder)
+	for i in set(links) & set(files):
+		print("path : ", i)
+	
+	while True:
+		# Главный цикл в котором выполняется скрипт.
+		links = list(link_founder)
+		files = list(scac_folder)
+		for i in set(files) - set(links):
+			os.link(i, PATH_FOR_LINKS+i.split("/")[-1])
+		time.sleep(TIME)
